0f784124d6dbc0dbad962c6bf885f9870f8610a0
n2sy <nidhal.jelassi@fsegt.utm.tn>
2024-06-10 12:59:52 +0100

    second step

---
+++ src/tasks/models/task.ts
@@ -0,0 +1,15 @@
+export class Task {
+  //   id: number;
+  //   name: string;
+  //   description: string;
+
+  constructor(
+    public id: string,
+    public title: string,
+    public description?: string,
+  ) {
+    // this.id = id;
+    // this.name = name;
+    // this.description = description;
+  }
+}
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,29 +1,41 @@
-import { Body, Controller, Get, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Req } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
+import { Task } from './models/task';
 
 @Controller('tasks')
 export class TasksController {
-  tab = [
-    {
-      id: 1,
-      title: 'Project 0',
-      description: 'My first project',
-    },
-  ];
+  tab = [new Task('1', 'Project 0', 'My first project')];
 
   @Get('all')
   getAllTasks() {
     return this.tab;
   }
+  @Get(':id')
+  getTaskById(@Param('id') id) {
+    return this.tab.find((task) => task.id === id);
+  }
 
   @Post('new')
   addTask(@Req() request, @Body() body) {
     console.log(request, body);
-    this.tab.push({
-      id: this.tab[this.tab.length - 1].id + 1,
-      ...body,
+    this.tab.push(
+      new Task(uuidv4(), body.title, body.description),
+
       //   title: body.title,
       //   description: body.description,
-    });
+    );
+    return { message: 'task added', tab: this.tab };
+  }
+
+  @Post('new/v2')
+  addTask2(@Req() request, @Body('title') name, @Body('description') desc) {
+    console.log(request);
+    this.tab.push(
+      new Task(uuidv4(), name, desc),
+
+      //   title: body.title,
+      //   description: body.description,
+    );
     return { message: 'task added', tab: this.tab };
   }
 }
