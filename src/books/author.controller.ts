11ea9bb58920073958d4f43657be69c1bb8e9faf
n2sy <nidhal.jelassi@fsegt.utm.tn>
2024-06-12 14:35:56 +0100

    add author controller and service

--- src/books/author.controller.ts
+++ src/books/author.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+} from '@nestjs/common';
+import { AuthorService } from './author.service';
 
 @Controller('author')
-export class AuthorController {}
+export class AuthorController {
+  constructor(private authSer: AuthorService) {}
+
+  @Get('all')
+  async getAllAuthors() {
+    let res = await this.authSer.getAllAuthorsServ();
+    return res;
+  }
+
+  @Get(':id')
+  async getAuthor(@Param('id', ParseIntPipe) id) {
+    let res = await this.authSer.getAuthorServ(id);
+    return res;
+  }
+
+  @Post('add')
+  async addAuthor(@Body() body) {
+    let res = await this.authSer.addAuthorServ(body);
+    return res;
+  }
+}
--- src/books/author.service.ts
+++ src/books/author.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AuthorEntity } from './entities/author.entity';
 
 @Injectable()
-export class AuthorService {}
+export class AuthorService {
+  constructor(
+    @InjectRepository(AuthorEntity) private authRepo: Repository<AuthorEntity>,
+  ) {}
+
+  addAuthorServ(nAuthor) {
+    return this.authRepo.save(nAuthor);
+  }
+
+  getAllAuthorsServ() {
+    return this.authRepo.find();
+  }
+
+  getAuthorServ(id) {
+    return this.authRepo.find({
+      where: {
+        id: id,
+      },
+    });
+  }
+}
